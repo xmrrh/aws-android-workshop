c64990c6bed47f84437772de92a61a4d3fce7628
go4real <go4real@gmail.com>
2019-09-24 14:03:23 -0700

    Add checkSession Method

--- app/src/main/java/com/example/socialandroidapp/CommonAction.java
+++ app/src/main/java/com/example/socialandroidapp/CommonAction.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.Callback;
 import com.amazonaws.mobile.client.UserStateDetails;
+import com.amazonaws.mobile.client.UserStateListener;
+import com.amazonaws.mobile.client.results.SignInResult;
 
 public class CommonAction {
     public static void openMain(Context context){
@@ -45,6 +47,28 @@ public class CommonAction {
             @Override
             public void onError(Exception e) {
                 Log.e("INIT", e.toString());
+            }
+        });
+    }
+
+    public static void checkSession(Context context) {
+        // Add code here
+        AWSMobileClient.getInstance().addUserStateListener(new UserStateListener() {
+            @Override
+            public void onUserStateChanged(UserStateDetails userStateDetails) {
+                switch (userStateDetails.getUserState()){
+                    case SIGNED_OUT:
+                        // user clicked signout button and signedout
+                        Log.i("userState", "user signed out");
+                        break;
+                    case SIGNED_OUT_USER_POOLS_TOKENS_INVALID:
+                        Log.i("userState", "need to login again.");
+                        //Alternatively call .showSignIn()
+                        CommonAction.openAuthMain(context);
+                        break;
+                    default:
+                        Log.i("userState", "unsupported");
+                }
             }
         });
     }
