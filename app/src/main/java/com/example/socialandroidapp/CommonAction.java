021e29ec6700fb631ed5a6691f567c6aa3b7a4ad
go4real <go4real@gmail.com>
2019-09-24 13:32:19 -0700

    Add initCognito

--- app/src/main/java/com/example/socialandroidapp/CommonAction.java
+++ app/src/main/java/com/example/socialandroidapp/CommonAction.java
@@ -2,6 +2,11 @@ package com.example.socialandroidapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.UserStateDetails;
 
 public class CommonAction {
     public static void openMain(Context context){
@@ -16,6 +21,32 @@ public class CommonAction {
         Intent intent=new Intent(context, targetClass);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(intent);
+    }
+
+    public static void initCognito(Context context) {
+        // Add code here
+        AWSMobileClient.getInstance().initialize(context.getApplicationContext(), new Callback<UserStateDetails>() {
+            @Override
+            public void onResult(UserStateDetails userStateDetails) {
+                switch (userStateDetails.getUserState()){
+                    case SIGNED_IN:
+                        // Open Main Activity
+                        CommonAction.openMain(context);
+                        break;
+                    default:
+                        AWSMobileClient.getInstance().signOut();
+                        CommonAction.openAuthMain(context);
+                        break;
+                }
+
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e("INIT", e.toString());
+            }
+        });
     }
 
 }
