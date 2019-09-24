f06c1ad415feb411108d6d330d9fbdb8e33a1449
go4real <go4real@gmail.com>
2019-09-24 13:55:05 -0700

    Refactor SplashActivity

--- app/src/main/java/com/example/socialandroidapp/SplashActivity.java
+++ app/src/main/java/com/example/socialandroidapp/SplashActivity.java
@@ -1,34 +1,17 @@
 package com.example.socialandroidapp;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.Callback;
-import com.amazonaws.mobile.client.UserStateDetails;
-
 public class SplashActivity extends AppCompatActivity {
-
-    private static final String TAG = SplashActivity.class.getSimpleName();
-
-    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        context = this;
-        _initCognito();
-    }
-
-    private void _initCognito() {
-        // Add code here
-
+        CommonAction.initCognito(this);
     }
 
 }
